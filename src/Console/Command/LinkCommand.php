0ae01e733cea7bece6502cc1c5f2af1fc6fccf46
Dieter Holvoet <dieter.holvoet@gmail.com>
2019-12-13 17:27:51 +0100

    Change link command to create a relative symlink

--- src/Console/Command/LinkCommand.php
+++ src/Console/Command/LinkCommand.php
@@ -13,15 +13,6 @@ class LinkCommand extends Command
 {
     public const COMMAND_NAME = 'link';
 
-    /** @var Filesystem */
-    protected $filesystem;
-
-    public function __construct()
-    {
-        parent::__construct();
-        $this->filesystem = new Filesystem();
-    }
-
     protected function configure()
     {
         $this
@@ -33,8 +24,13 @@ class LinkCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $source = __DIR__ . '/../../../.php_cs.php';
-        $destination = getcwd() . '/.php_cs.dist';
+        $fileName = '.php_cs.php';
+        $packageRoot = __DIR__ . '/../../..';
+        $projectRoot = $this->getComposerRoot();
+        $relativePackageRoot = substr(realpath($packageRoot), strlen(realpath($projectRoot)));
+
+        $source = '.' . implode('/', [$relativePackageRoot, $fileName]);
+        $destination = implode('/', [$projectRoot, $fileName]);
 
         if (file_exists($destination)) {
             if ($io->confirm("An existing config is found at {$destination}. Overwrite?")) {
@@ -45,12 +41,45 @@ class LinkCommand extends Command
         }
 
         try {
-            $this->filesystem->symlink($source, $destination);
+            echo $this->getRelativePath($source, $destination);
+            exec("ln -s {$source} ./{$fileName}}");
         } catch (IOException $e) {
             $io->error($e->getMessage());
             return;
         }
 
         $io->success('Successfully linked the php-cs-fixer config to ' . $destination);
+    }
+
+    protected function getComposerRoot(): ?string
+    {
+        $composerRoot = null;
+        $dir = __DIR__;
+
+        do {
+            if (
+                file_exists($dir . '/composer.json')
+                && dirname($dir) !== 'wmcodestyle'
+            ) {
+                $composerRoot = $dir;
+            }
+
+            $dir = realpath($dir . '/..');
+        } while ($dir !== '/');
+
+        return $composerRoot;
+    }
+
+    protected function getRelativePath(string $from, string $to): string
+    {
+        $dir = explode(DIRECTORY_SEPARATOR, is_file($from) ? dirname($from) : rtrim($from, DIRECTORY_SEPARATOR));
+        $file = explode(DIRECTORY_SEPARATOR, $to);
+
+        while ($dir && $file && ($dir[0] == $file[0])) {
+            array_shift($dir);
+            array_shift($file);
+        }
+
+        return str_repeat('..' . DIRECTORY_SEPARATOR, count($dir)) . implode(DIRECTORY_SEPARATOR, $file);
     }
 }
