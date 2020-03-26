cd1872752c5d82f7b559eda9f909a2e537161b16
Dieter Holvoet <dieter.holvoet@gmail.com>
2020-03-26 12:02:31 +0100

    #6 Add phpstan

---
+++ src/PHPStan/Reflection/MethodsFromPresentersExtension.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Wieni\wmcodestyle\PHPStan\Reflection;
+
+use PHPStan\Analyser\OutOfClassScope;
+use PHPStan\Type\FileTypeMapper;
+
+class MethodsFromPresentersExtension implements MethodsClassReflectionExtension
+{
+    /** @var FileTypeMapper */
+    private $fileTypeMapper;
+
+    public function __construct(
+        FileTypeMapper $fileTypeMapper
+    ) {
+        $this->fileTypeMapper = $fileTypeMapper;
+    }
+
+    public function hasMethod(ClassReflection $classReflection, string $methodName): bool
+    {
+        if (!$modelClassReflection = $this->getPresenterModel($classReflection)) {
+            return false;
+        }
+
+        return $modelClassReflection->hasMethod($methodName);
+    }
+
+    public function getMethod(ClassReflection $classReflection, string $methodName): MethodReflection
+    {
+        return $this->getPresenterModel($classReflection)->getMethod($methodName, new OutOfClassScope);
+    }
+
+    protected function getPresenterModel(ClassReflection $classReflection): ?ClassReflection
+    {
+        if (!$classReflection->getAncestorWithClassName('')) {
+            return null;
+        }
+
+        $fileName = $classReflection->getFileName();
+
+        if ($fileName === false) {
+            return null;
+        }
+
+        $docComment = $classReflection->getNativeReflection()->getDocComment();
+
+        if ($docComment === false) {
+            return null;
+        }
+
+        $resolvedPhpDoc = $this->fileTypeMapper->getResolvedPhpDoc($fileName, $classReflection->getName(), null, null, $docComment);
+        $propertyTags = $resolvedPhpDoc->getPropertyTags();
+
+        if (!isset($propertyTags['entity'])) {
+            return null;
+        }
+
+        return $propertyTags['entity']->getType()->getClassReflection();
+    }
+}
