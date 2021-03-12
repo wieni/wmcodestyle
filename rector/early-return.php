fcba90d139580697a2469ad718a7ae970c97af8e
Dieter Holvoet <dieter.holvoet@gmail.com>
2021-03-12 13:54:34 +0100

    Update Rector rules

--- rector/early-return.php
+++ rector/early-return.php
@@ -3,7 +3,6 @@
 declare (strict_types=1);
 
 use Rector\EarlyReturn\Rector\Foreach_\ChangeNestedForeachIfsToEarlyContinueRector;
-use Rector\EarlyReturn\Rector\If_\ChangeAndIfToEarlyReturnRector;
 use Rector\EarlyReturn\Rector\If_\ChangeIfElseValueAssignToEarlyReturnRector;
 use Rector\EarlyReturn\Rector\If_\ChangeNestedIfsToEarlyReturnRector;
 use Rector\EarlyReturn\Rector\If_\ChangeOrIfReturnToEarlyReturnRector;
@@ -13,7 +12,6 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 return static function (ContainerConfigurator $containerConfigurator) : void {
     $services = $containerConfigurator->services();
     $services->set(ChangeNestedForeachIfsToEarlyContinueRector::class);
-    $services->set(ChangeAndIfToEarlyReturnRector::class);
     $services->set(ChangeIfElseValueAssignToEarlyReturnRector::class);
     $services->set(ChangeNestedIfsToEarlyReturnRector::class);
     $services->set(RemoveAlwaysElseRector::class);
